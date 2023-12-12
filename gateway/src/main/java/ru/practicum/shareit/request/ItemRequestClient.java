318811c6d7bbe323a86d7ba814e2eb95972b633b
Egor <Jeglederefter@yandex.ru>
2023-12-12 20:06:58 +0300

    feat: TZ env variable added to docker-compose.yaml

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -18,7 +18,7 @@ public class ItemRequestClient extends BaseClient {
     private static final String API_PREFIX = "/requests";
 
     @Autowired
-    public ItemRequestClient(@Value("http://localhost:9090") String serverUrl, RestTemplateBuilder builder) {
+    public ItemRequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
