070dc4df6249257a06ef0c74ab364a05d90fd82d
Egor <Jeglederefter@yandex.ru>
2023-12-16 14:25:20 +0300

    feat: unused imports removed

--- gateway/src/main/java/ru/practicum/shareit/error/ErrorResponse.java
+++ gateway/src/main/java/ru/practicum/shareit/error/ErrorResponse.java
@@ -1,12 +1,5 @@
 package ru.practicum.shareit.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.shareit.user.exception.InvalidEmailException;
-import ru.practicum.shareit.user.exception.UserDoesNotExistException;
-
 public class ErrorResponse {
 
     private final String error;
